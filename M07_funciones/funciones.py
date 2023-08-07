c5ee8f8516ca115ef7922b869b21cc412ff0a9b5
Cesar Mora <ingeniotekcol@gmail.com>
2023-08-06 19:17:41 -0500

    Module 07 finished

--- M07_funciones/funciones.py
+++ M07_funciones/funciones.py
@@ -1,4 +1,4 @@
-class CasaBarrio:
+""" class CasaBarrio:
     def __init__(self, numero_casa, fachada):
         self.numero_casa = numero_casa
         self.fachada = fachada
@@ -16,3 +16,43 @@ casa1.imprimirCasa()
 casa2.imprimirCasa()
 
 print(casa1.imprimirCasa2())
+
+def factorial (number):
+    '''
+    Calcula el factorial de un numero entero positivo
+    '''
+    if number >1 :
+        number = number * factorial(number -1)
+    return number
+
+print(factorial(5))
+help(factorial)
+ """
+
+x1 = [10, 20, 30]
+def funcion(entrada):
+    entrada.append(40)
+
+x = x1.copy()
+funcion(x)
+print(x1)
+print(x)
+
+
+def funcion(entrada):
+    entrada = 0
+    print(id(entrada))
+x = 1
+print(id(x))
+funcion(x)
+
+
+def funcion(entrada):
+    entrada.append(40)
+    print(id(entrada))
+x = [10, 20, 30]
+print(id(x))
+funcion(x)
+
+lambda_producto = lambda x, y: x * y
+print(lambda_producto(3, 4))
